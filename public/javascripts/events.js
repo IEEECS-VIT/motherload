72c0d0f195c09f57161f13292a3b7295c8be8262
TanishaChawla <chawlatanisha20@gmail.com>
2016-05-02 20:32:08 +0530

    events

---
+++ public/javascripts/events.js
@@ -0,0 +1,177 @@
+var events=[
+    {
+        'name':"Workshops",
+        'url': "https://res.cloudinary.com/events-gravitas/image/upload/v1444243710/prev/ios2.jpg"
+    },
+    {
+        'name':"Builtrix",
+        'url':"https://res.cloudinary.com/events-gravitas/image/upload/v1444243708/prev/jenga.jpg"
+    },
+    {
+        'name':'Applied Engineering',
+        'url':'https://res.cloudinary.com/events-gravitas/image/upload/v1444243740/prev/robotix2.jpg'
+    },
+    {
+        'name':'Bits and Bytes',
+        'url':'https://res.cloudinary.com/events-gravitas/image/upload/v1444243709/prev/jumble.jpg'
+    },
+    {
+        'name':'Circuit',
+        'url':'https://res.cloudinary.com/events-gravitas/image/upload/v1444243738/prev/robotix1.jpg'
+    },
+    {
+        'name':'Quiz',
+        'url':'https://res.cloudinary.com/events-gravitas/image/upload/v1444243725/prev/quiz1.jpg'
+    },
+    {
+        'name':'Robotics',
+        'url':'https://res.cloudinary.com/events-gravitas/image/upload/v1444243740/prev/robotix2.jpg'
+    },
+    {
+        'name':'Management/ Debate/ Discussion',
+        'url':'https://res.cloudinary.com/events-gravitas/image/upload/v1444243707/prev/ios3.jpg'
+    },
+    {
+        'name':'Bio/Chemical',
+        'url':'https://res.cloudinary.com/events-gravitas/image/upload/v1444243731/prev/random8.jpg'
+    },
+    {
+        'name':'Science and Tech',
+        'url':'https://res.cloudinary.com/events-gravitas/image/upload/v1444243714/prev/contraptions.jpg'
+    },
+    {
+        'name':'Informals',
+        'url':'https://res.cloudinary.com/events-gravitas/image/upload/v1444243677/prev/dynasty.jpg'
+    },
+    {
+        'name':'Online',
+        'url':'http://res.cloudinary.com/events-gravitas/image/upload/v1444243746/prev/young3.jpg'
+    },
+    {
+        'name':'SSIC',
+        'url':'http://res.cloudinary.com/events-gravitas/image/upload/v1444243745/prev/young2.jpg'
+    }
+];
+var workshops=[
+    { 'subcat':'Matlab',
+        'club': 'ASME VIT,ISTE',
+        'description':"MATLAB- The MATLAB platform is optimized for solving engineering and scientific problems. The matrix-based MATLAB language is the world’s most natural way to express computational mathematics.A great resource for the students interested in control design of mechanical systems.Mechanical Engineers of the Design and Manufacturing fields use  MATLAB heavily.You can do almost everything in MATLAB, any simulation, plotting, coding, making GUI. That makes it one of the most used software in all graduate schools. Solving assignments, writing codes, plotting graphs, simulating models.MATLAB will be taught according to Mechanical based operations."+
+        "Curriculum"+
+            "Day 1: Details of Matlab tools and hand's on session on Matlab"
+
+    },
+    {
+        'subcat':'Game Dev',
+        'club':'Computer Society of India-VIT',
+        'description':'Project X is to provide a platform for game developers to come up with games, in a limited amount of time. But the event is for both gamers and the aspiring game developers. This is a 3 day Game development workshop. The first two days will be given to all the teams (or individuals) to prototype a game. The games can range from anything from PC games to console games to even arcade games. On the last day we open the floor to gamers or anyone who wants to try out these games.'
+    },
+    {
+        'subcat':'Web Dev',
+        'club':'IEEE-CS VIT',
+        'description':'24 Hour continuous workshop-cum-hackathon on web Development taken by the Mozilla Developers, which provides the golden opportunity to the freshers to get the insight knowledge of web dev by the developers. An exposure to vast Mozilla respositories and APIs (of firefox OS). Participants will learn HTML and CSS from basics and will also be exposed to the website development procedures.'
+    },
+    {
+        'subcat':'Cloud',
+        'club':'IEEE-WIE',
+        'description':"Cloud Computing : Understanding the concepts of cloud computing and it's applications. How to implement this knowledge into projects at college level."
+    },
+    {
+        'subcat':'Photography',
+        'club':'Phtography Club',
+        'description':'"A photograph is a secret about a secret. The more it tells you the less you know.Introduction: Kodachrome is a photography workshop where the basics of nature and wildlife photography will be taught by a renowned photographer.'+ 'Description'+
+'•	A three to four hour workshop on nature and wildlife photography will be conducted.'+
+'•	The expert to teach the participants, from scratch to advanced level of photography.'+
+'•	To be followed by, an on-spot photography event and to be judged by the expert."'
+    },
+    {
+        'subcat':'Cryptography',
+        'club':'SIAM',
+        'description':'"CRYPTOGRAPHY WORKSHOP.'+
+
+
+        'A completely technical workshop on cryptography that is basically a study on secure communication in the presence of third parties called adversaries.'+
+            'External speaker is going to brief and help students code the "Secret Code". The lecture will comprise of teaching the practise and'+
+    'study of ethics of secret communication.'+
+        'Major topics will comprise of Micro dots, Crypto scheme comparison, Merging words with images, and other words to hide information in'+
+
+'storage or transit, scrambling plain text into ciphertext. It is majorly in used in the current era because of its ability to'+
+'securely store and transfer sensitive information and hence has proved a critical factor in success in war and business.'+
+ '   A complete interactive session with remarkable learning experience."'
+    },
+    {
+        'subcat':'Ui/Ux',
+        'club':'VIT ANIMATION CLUB',
+        'description':'"Ui/UX skills are gaining significance day by day. The number of people/professionals  i.e engineers without much knowledge in design tools like Photoshop are very less, making it essential for an individual to learn these skills. The workshop aims at educating/empowering the participants with the knowledge of the following software:'+
+        'Adobe Master'+
+'•	Photoshop'+
+'•	Illustrator'+
+'•	After Effects'+
+'•	Indesign"'
+    },
+    {
+        'subcat':'DIP',
+        'club':'IEEE-WIE IEEE-SPS',
+        'description':'"Digital image processing is the processing of image using mathematical operation by using any form of signal processing which is an image  a series of images,or a video; the output of image processing may be either an image or a set of parameter related to the image.'+
+        'image processing is used in many fields like computer graphics ,pattern recognition,robotics, artificial intelligence etc. So this workshop must interest the student who wants to have a basic knowledge of it."'
+
+    },
+    {
+        'subcat':'Application Development',
+        'club':'ACM VIT',
+        'description':'"Python and Google App Engine Workshop'+
+        'This workshop will begin with the basics of Python.  Participants will then be introduced to Google App Engine, and be shown the benefits of GAE. They will be encouraged to develop their own web app and then be shown how to deploy the app on a Google developer console, with a unique URL."'
+    },
+    {
+        'subcat':'Computer Hardware',
+        'club':'Computer Society of India-VIT',
+        'description':'Under The Hood is a workshop where students will be taught how to assemble their own computers. The students will get to know how to purchase necessary parts, understand how they work together and eventually learn the skills involved in building a Personal Computer. This workshop will be open to students of all branches. The students will also take turns and work with the various components that go into building the computer.'
+    },
+    {
+        'subcat':'Comic Design,Eng sketching',
+        'club':'Creativity Club',
+        'description':'In this workshop the speaker will demonstrate how to create comic designs from the basics. All the materials will be provided by us and the participants get to design creative cartoons.'
+    },
+    {
+        'subcat':'3D printing',
+        'club':'ESquare-Engineers and Entrepreneurs',
+        'description':'"3-D Printing has been dubbed as the technology of the future! Indeed it has many reasons that advocate the stance. It is currently the best rapid manufacturing technology for personalised manufacturing and rapid prototyping and is slowly finding its way into the industry. It has been predicted that it will be the harbinger of the 3rd industrial revolution.'+
+        'To familiarise students with this modernistic and cutting-edge technology and to teach them about the basics of 3-D Printing, is the desideratum of this workshop. The workshop will be attended by students from a large verity of branches including computer, electrical, IT, mechanical, civil and even biotech engineering! This goes to show the vast appeal of this technology.'+
+        'The workshop shall have distinguished guests coming from international companies. The introductory session, consisting of basics of 3-D Printing and additive manufacturing, processes, materials and printers used in the process, 3-D Printers set up during the workshop will demostrate the technology to the students live. The next session will introduce the participants to basics of CAD software designing and slicing technique. The students will be given several useful softwares and taught to design their own objects. The third session, focusses specifically on Metal RP (Rapid Prototyping). The students shall be shown several high grade metal components made using titanium and aluminium. After the lunch break, the fourth session, consisting of 3-D scanning, will showcase state of the art 3-D scanners. At the end the students will have the hands-on sessions with the scanners and printers. This is the most interesting and exciting part for the participants, because most of them haven’t seen or used a 3-D Printer before. The participants also get their designed models 3-D printed given to them as mementos!'+
+    'They will surely looking forward to explore more frontiers of this very unique technology!"'
+
+    }
+];
+$(document).ready(function(){
+
+   var ev=document.getElementById('events');
+    var l=events.length;
+    var a='<h3>EVENTS</h3><div class="container"><div class="row">';
+    for(var i=0;i<l;i++)
+    {
+        a+='<div class="col s12 l4 m6"><div class="card small" onclick="display(\''+events[i].name+'\')"><div class="card-image waves-effect waves-block waves-light">'+
+            '<img class="activator" src="'+events[i].url+'"></div><div class="card-content"><span class="card-title activator grey-text text-darken-4">'+events[i].name+'</span>'+
+            '</div></div></div>'
+    }
+    a+='</div></div>';
+    ev.innerHTML=a;
+
+});
+function display(a)
+{
+    $('#events').hide();
+    if(a=='Workshops')
+    {
+        var x='<h5>Workshops</h5><div class="container"><div class="row">';
+        for(var i=0;i<workshops.length;i++)
+        {
+            x+='<div class="col s12 l4 m6"><div class="card small">'+'<div class="card-image waves-effect waves-block waves-light"><img class="activator" src="images/office.jpg">'+
+        '</div><div class="card-content"><span class="card-title activator grey-text text-darken-4">'+workshops[i].subcat+'<i class="material-icons right">more_vert</i></span><hr>'+
+        '<p><a href="#">Register</a></p>'+'</div><div class="card-reveal"><span class="card-title grey-text text-darken-4">'+workshops[i].subcat+'<i class="material-icons right">close</i></span>'+
+        '<h6 class="club">'+workshops[i].club+'</h6><p class="grey-text text-darken-3">'+workshops[i].description+'</p></div></div></div>';
+        }
+        x+='</div></div>';
+        document.getElementById('category').innerHTML=x;
+
+    }
+
+}
