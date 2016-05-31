d0e46a29c112fba47773eb6849559059b8811160
sakshianand <sakku.anand@gmail.com>
2016-06-01 02:51:56 +0530

    preloader added

--- public/javascripts/home2.js
+++ public/javascripts/home2.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+    setTimeout(function(){
+        $('body').addClass('loaded');
+        $('h1').css('color','#222222');
+    }, 3000);
     var
         $gravitas = $(".gravitas"),
         $gravitasSection = $(".gravitas-section"),
