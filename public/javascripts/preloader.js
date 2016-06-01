91d5f60a8b99fd5a72b01cfe7eb11f4e9814a247
sakshianand <sakku.anand@gmail.com>
2016-06-01 11:41:35 +0530

    reduntant files removed

--- public/javascripts/preloader.js
+++
@@ -1,8 +0,0 @@
-$(window).load(function() {
-    $("#status").fadeIn();
-    $("#preloader").fadeIn();
-    $.get(url, data, function () {
-        $("#status").fadeOut();
-        $("#preloader").fadeOut();
-    });
-})
